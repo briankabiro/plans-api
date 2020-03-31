270e1b7bdb290215e144a25034f01f178d08ee1c
briankabiro <briankigondu@gmail.com>
2020-03-31 10:00:11 +0300

    Remove requirement for passing codename to retrieve token
    * the API required a codename to be sent with the payload in order
    to retrieve a token

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -2,38 +2,30 @@ class MembersController < ApplicationController
   before_action :authorize, only: [:create, :update]
 
   def create
-    member = Member.new(member_params)
+    member = Member.create(member_params)
 
-    if member.save
-      render json: { message: 'Member created successfully' }, status: :created 
+    if member.persisted?
+      render json: { message: 'Member created successfully', token: JsonWebToken.encode(member.first_name) }, status: :created
     else
-      render json: { errors: member.errors.full_messages }, status: :bad_request 
-    end
-  end
-
-  def signup
-    if params[:codename] == codename
-      auth_token = JsonWebToken.encode(codename)
-      render json: { auth_token: auth_token }, status: :ok
-    else
-      render json: { "message": "invalid codename"}, status: :bad_request
+      render json: { errors: member.errors.full_messages }, status: :bad_request
     end
   end
 
   def assign_plan
     @member = Member.find(params[:id])
-    @member.plan_id = params[:plan_id]
-    @member.save
 
-    render json: @member
+    @member.update_attributes(plan_id: params[:plan_id])
+
+    if @member.persisted?
+      render json: { message: 'Plan assigned successfully', member: @member }
+    else
+      render json: { message: 'Failed to assign plan' }
+    end
   end
 
   private
+
   def member_params
     params.permit(:first_name, :last_name, :date_of_birth, :codename)
-  end
-
-  def codename
-    "kittens"
   end
 end
