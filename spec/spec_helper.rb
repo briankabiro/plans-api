2f2e75d2cb140251eca5fc27a5ea44e28fcfe735
briankabiro <briankigondu@gmail.com>
2020-03-31 10:01:37 +0300

    Update SimpleCov configuration
    * add a filter that only includes coverage for file with `spec`
    in the path

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,7 +15,10 @@
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 
 require 'simplecov'
-SimpleCov.start
+SimpleCov.start 'rails' do
+  add_filter %r{^/spec/}
+end
+
 require 'coveralls'
 Coveralls.wear!
 
